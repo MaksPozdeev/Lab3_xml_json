8ad9cd3cdeef8e04b22acc55fe5c7f018e91c776
Maksim <maximglazov18@gmail.com>
2019-10-08 18:04:18 +0400

    JSON output to file implemented in MainParser; MainParser used: try with resources

--- src/main/java/parser/MainParser.java
+++ src/main/java/parser/MainParser.java
@@ -7,10 +7,7 @@ import entity.Products;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 
 public class MainParser {
@@ -27,9 +24,33 @@ public class MainParser {
         }
 
         if (products != null) {
-            System.out.println(objToJson(products));
+            String jsonString = objToJson(products);
+            System.out.println(jsonString);
+            int q = saveJsonInFile(jsonString, "NewJsonFile");
+            System.out.println("q = " + q);
         }
 
+    }
+
+    private static Integer saveJsonInFile(String jsonString, String nameJson) {
+        int result = -1;
+        if (jsonString == null) {
+            return -1;
+        }
+        String newJsonFileName;
+        if (nameJson.isEmpty()) {
+            newJsonFileName = "NewJsonFile.json";
+        } else {
+            newJsonFileName = nameJson + ".json";
+        }
+        try (FileWriter writer = new FileWriter(newJsonFileName, false)) {
+            writer.write(jsonString);
+            writer.flush();
+            result = 1;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     private static String objToJson(Products products) {
@@ -45,7 +66,6 @@ public class MainParser {
             Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
             FileReader reader = new FileReader(xmlFilePath);
             products = (Products) unmarshaller.unmarshal(reader);
-
         } catch (JAXBException | FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -53,28 +73,19 @@ public class MainParser {
     }
 
     private static String readUsingBufferedReader(String fileName) {
-        BufferedReader reader = null;
+
         StringBuilder stringBuilder = new StringBuilder();
 
-        try {
-            reader = new BufferedReader(new FileReader(fileName));
-            String line = null;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
             String ls = System.getProperty("line.separator");
             while ((line = reader.readLine()) != null) {
                 stringBuilder.append(line);
                 stringBuilder.append(ls);
             }
-            // delete the last ls
             stringBuilder.deleteCharAt(stringBuilder.length() - 1);
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            if (reader != null)
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
         }
 
         return stringBuilder.toString();
